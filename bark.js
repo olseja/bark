64a910ff95dacdc39fd64b5c0714942126e9f6d0
Aleksandra Marek <aleksandra.marek@outlook.com>
2017-09-06 22:29:33 +0200

    bark3

--- bark.js
+++ bark.js
@@ -1,12 +1,12 @@
-function bark (name, weight) {
-	if (weight > 20) {
-		console.log (name + " szczeka HAU HAU");
-	} else {
-		console.log (name + " szczeka hau hau");
-	}
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " szczeka HAU HAU");
+    } else {
+        console.log(name + " szczeka hau hau");
+    }
 }
 
-bark ("Burek", 23);
-bark ("Mops", 13);
-bark ("Reksio", 53);
-bark ("Saba", 17);
+bark("Burek", 23);
+bark("Mops", 13);
+bark("Reksio", 53);
+bark("Saba", 17);
